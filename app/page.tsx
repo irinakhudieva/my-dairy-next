ce904cf916ea92899d47d677a8f104f9dc9d4e7d
irinakhudieva <119666740+irinakhudieva@users.noreply.github.com>
2024-01-12 11:55:58 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,65 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import DayState from '@/components/DayState';
 
 export default function Home() {
+
+  const duties = {
+    // "daily leetcode": {
+    //   "10.11.23": true,
+    //   "11.11.23": false,
+    //   "12.11.23": true,
+    // },
+    // "daily React/Next JS": {
+    //   "10.11.23": true,
+    //   "11.11.23": true,
+    //   "12.11.23": false,
+    // },
+    // "daily JavaScript": {
+    //   "10.11.23": true,
+    //   "11.11.23": false,
+    //   "12.11.23": false,
+    // },
+  }
+  const today = new Date()
+  const todayWeekDay =  today.getDay()
+
+  const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  const sortedWeekDays = weekDays.slice(todayWeekDay).concat(weekDays.slice(0, todayWeekDay));
+  
+  const last7Day = weekDays.map((_, index) => {
+    const date = new Date()
+    date.setDate(date.getDate() - index)
+    return date.toISOString().slice(0, 10)
+  }).reverse()
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='container relative flex flex-col gap-8 px-4 pt-8'>
+      {duties === null || (Object.keys(duties).length === 0 && (
+        <h1 className="mt-20 text-4xl font-light text-white font-display text-center">
+          You have no registered duty
+        </h1>
+      ))}
+
+      {duties !== null  && Object.entries(duties).map(([duty]) => 
+        <div key={duty} className="flex flex-col gap-2">
+            <div className="flex justify-between item-center">
+              <span className="text-xl font-light text-white font-display">{duty}</span>
+              <button>
+                <Image src='/delete.svg' width={20} height={20} alt="Icon Delete" />
+              </button>
+            </div>
+
+            <section className="grid grid-cols-7 bg-neutral-800 rounded-md p-2">
+                {weekDays.map(day => (
+                  <div key={day} className="flex flex-col">
+                    <span className="font-sans text-center text-xs text-white">{day}</span>
+                    <DayState day={true} />
+                  </div>
+                ))}
+            </section>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
   )
 }
